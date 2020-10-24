5ddb2ecb0bcf559a04298c5c4f302935fadc45dc
caiosdf <caiosdf15@gmail.com>
2020-10-24 20:39:47 -0300

    + agenda

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1339),
-  url: 'https://admin.umnovoolhar.art.br'
+  //url: 'https://admin.umnovoolhar.art.br'
 });
